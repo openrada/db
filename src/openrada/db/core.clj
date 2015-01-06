515f85a3b2e075a21ea15baeb6059a3e906080cb
Anton Podviaznikov <podviaznikov@gmail.com>
2015-01-05 23:37:28 -0800

    add more queries

--- src/openrada/db/core.clj
+++ src/openrada/db/core.clj
@@ -2,6 +2,14 @@
   (:require [rethinkdb.core :refer [connect close]]
             [rethinkdb.query :as r]))
 
+(def base-url "https://api.openrada.com/v1/parliament/")
+
+(defn add-url [ent-name ent convocation]
+  (assoc ent :url (str base-url convocation "/" ent-name "/" (:id ent))))
+
+(defn add-urls-to-all [ents ent-name convocation]
+  (map #(add-url ent-name % convocation) ents))
+
 (defn find-first
   [f coll]
   (first (filter f coll)))
@@ -30,6 +38,11 @@
                        (r/without row [field])))
       (r/run (:connection db))))
 
+(defn get-by-id [db tablename id]
+  (-> (my-db-table tablename)
+      (r/get id)
+      (r/run (:connection db))))
+
 ; members
 
 (defn save-members [db members]
@@ -50,18 +63,8 @@
   (-> memberst
       (r/get-all [convocation] {:index "convocation"})
       (r/without [:image])
-      (r/eq-join "committee_id" committeest)
-      (r/zip)
-      (r/eq-join "faction_id" factionst)
-      (r/zip)
-      (r/run (:connection db))))
-
-
-(defn get-member [db id]
-  (->  memberst
-      (r/get id)
-      (r/run (:connection db))))
-
+      (r/run (:connection db))
+      (add-urls-to-all "members" convocation)))
 
 
 (defn get-member-by-short-name [db short-name]
@@ -89,7 +92,8 @@
 (defn get-committees-from-convocation [db convocation]
   (-> committeest
       (r/get-all [convocation] {:index "convocation"})
-      (r/run (:connection db))))
+      (r/run (:connection db))
+      (add-urls-to-all "committees" convocation)))
 
 
 ; factions
@@ -103,35 +107,100 @@
 (defn get-factions-from-convocation [db convocation]
   (-> factionst
       (r/get-all [convocation] {:index "convocation"})
-      (r/run (:connection db))))
+      (r/run (:connection db))
+      (add-urls-to-all "factions" convocation)))
 
 
 ;; compount queries
 
 
-(defn get-members-full [db convocation]
-  (let [members (get-members-from-convocation db convocation)
-        factions (get-factions-from-convocation db convocation)
-        committees (get-committees-from-convocation db convocation)]
-      (map (fn [member]
-             (let [faction (find-first #(= (:id %) (:faction_id member)) factions)
-                   f (assoc faction :role (:faction_role member))
-                   committee (find-first #(= (:id %) (:committee_id member)) committees)
-                   c (assoc committee :role (:committee_role member))
-                   m (dissoc member :faction_id
-                                    :faction_role
-                                    :committee_id
-                                    :committee_role)]
-               (assoc m :faction (dissoc f :convocation)
-                        :committee (dissoc c :convocation))
+(defn pure-member [member]
+  (dissoc member :faction_id
+                 :faction_role
+                 :committee_id
+                 :committee_role))
 
-               )
 
-             ) members)
+(defn enhance-member [member factions committees]
+  (let [faction (find-first #(= (:id %) (:faction_id member)) factions)
+        f (assoc faction :role (:faction_role member))
+        committee (find-first #(= (:id %) (:committee_id member)) committees)
+        c (assoc committee :role (:committee_role member))
+        m (pure-member member)]
+    (assoc m :faction (dissoc f :convocation)
+            :committee (dissoc c :convocation))))
 
-    )
-  )
+(defn get-members-full
+  ([db convocation]
+     (let [members (get-members-from-convocation db convocation)
+           factions (get-factions-from-convocation db convocation)
+           committees (get-committees-from-convocation db convocation)]
+       (get-members-full db convocation members factions committees)))
+  ([db convocation members factions committees]
+      (map #(enhance-member % factions committees) members)))
+
+
+(defn get-member-full
+  ([db convocation id]
+     (let [members (get-members-from-convocation db convocation)
+           factions (get-factions-from-convocation db convocation)
+           committees (get-committees-from-convocation db convocation)]
+       (get-member-full db convocation id factions committees)))
+  ([db convocation id factions committees]
+    (let [member (get-by-id db "members" id)]
+      (enhance-member member factions committees))))
+
+(defn enhance-faction [faction members]
+  (let [members (filter #(= (:id faction) (:faction_id %)) members)
+        ms (map #(assoc % :role (:faction_role %)) members)]
+    (assoc faction :members (map pure-member ms))))
+
+
+(defn get-factions-full
+  ([db convocation]
+     (let [members (get-members-from-convocation db convocation)
+           factions (get-factions-from-convocation db convocation)]
+       (get-factions-full db convocation members factions)))
+  ([db convocation members factions]
+      (map #(enhance-faction % members) factions)))
+
+
+(defn get-faction-full
+  ([db convocation id]
+     (let [members (get-members-from-convocation db convocation)
+           factions (get-factions-from-convocation db convocation)]
+       (get-faction-full db convocation id members)))
+  ([db convocation id members]
+    (let [faction (get-by-id db "factions" id)]
+      (enhance-faction faction members))))
+
+
+(defn enhance-committee [committee members]
+  (let [members (filter #(= (:id committee) (:committee_id %)) members)
+        ms (map #(assoc % :role (:committee_role %)) members)]
+    (assoc committee :members (map pure-member ms))))
+
+
+(defn get-committees-full
+  ([db convocation]
+     (let [members (get-members-from-convocation db convocation)
+           committees (get-committees-from-convocation db convocation)]
+       (get-committees-full db convocation members committees)))
+  ([db convocation members committees]
+      (map #(enhance-committee % members) committees)))
+
+
+(defn get-committee-full
+  ([db convocation id]
+     (let [members (get-members-from-convocation db convocation)
+           committees (get-committees-from-convocation db convocation)]
+       (get-committees-full db convocation id members)))
+  ([db convocation id members]
+     (let [committee (get-by-id db "committees" id)]
+       (enhance-committee committee members))))
 
 ;(def db {:connection (make-connection "127.0.01")})
-
+;(get-factions-from-convocation db 8)
 ;(get-members-full db 8)
+;(get-factions-full db 8)
+;(get-committees-full db 8)
